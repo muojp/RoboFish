be54c42130db483385e5f87bf25f757f495ad2bb
Sasaki Akira <akira@gclue.jp>
2017-03-11 17:00:27 +0900

    Update TestCode.

---
+++ Hardware/src/robofish.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*- 
+import smbus
+import time
+import threading
+import RPi.GPIO as GPIO
+import sys
+
+## DRV8830 Default I2C slave address
+SLAVE_ADDRESS  = 0x64
+
+''' DRV8830 Register Addresses '''
+## sample rate driver
+CONTROL = 0x00
+
+## Value motor.
+FORWARD = 0x01
+BACK = 0x02
+STOP = 0x00
+
+## Value of hall sensor.
+HALL_SENSOR = 22 #ホールセンサを22Pinに接続
+
+## smbus
+bus = smbus.SMBus(1)
+
+class Robot(threading.Thread):   
+    flg = True
+#class Robot():   
+    def __init__(self, address=SLAVE_ADDRESS): 
+        self.address = address
+        self.c = 0
+        threading.Thread.__init__(self)
+        GPIO.cleanup()
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup( HALL_SENSOR, GPIO.IN ) #GPIOを入力に設定
+        self.flg = True
+      
+    def run(self):
+        self.count()
+    
+    def map(self, x, in_min, in_max, out_min, out_max):
+        return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+
+    def get_reel_count(self):
+        return self.c
+    
+    def reset_reel_count(self):
+        self.c = 0
+    
+    # 指定した回転数になるまで回転数をカウント。
+    def count(self):
+        while self.flg:
+            while GPIO.input( HALL_SENSOR ):
+                pass
+            while not GPIO.input( HALL_SENSOR ):
+                pass
+            self.c += 1
+            sys.stdout.write("\rcount %d   " % self.c)
+            sys.stdout.flush()
+            time.sleep(0.01)
+                      
+    # speedは1-100で指定
+    def reel_forward(self, speed):
+        if speed < 0:
+            print "value is over 1,  must define 1-100 as speed."
+            return
+        elif speed > 100:
+            print "value is under 100,  must define 1-100 as speed."
+            return
+        s = self.map(speed, 1, 100, 1, 58)
+        sval = FORWARD | ((s+5)<<2) #スピードを設定して送信するデータを1Byte作成
+        bus.write_i2c_block_data(self.address,CONTROL,[sval]) #生成したデータを送信
+
+    def reel_stop(self):
+        bus.write_i2c_block_data(self.address,CONTROL,[STOP]) #モータへの電力の供給を停止(惰性で動き続ける)
+        
+    # speedは0-100で指定
+    def reel_back(self, speed):
+        if speed < 0:
+            print "value is over 1,  must define 1-100 as speed."
+            return
+        elif speed > 100:
+            print "value is under 100,  must define 1-100 as speed."
+            return
+        s= self.map(speed, 1, 100, 1, 58)
+        sval = BACK| ((s+5)<<2) #スピードを設定して送信するデータを1Byte作成
+        bus.write_i2c_block_data(self.address,CONTROL,[sval]) #生成したデータを送信
+
+    def reel_brake(self):
+        bus.write_i2c_block_data(self.address,CONTROL,[0x03]) #モータをブレーキさせる
+        
